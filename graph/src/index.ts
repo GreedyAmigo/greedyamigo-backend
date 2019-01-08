2cfb02b8c5e459bdde34db336347c6882833d8ec
Simon Ammer <ammersimon@gmail.com>
2019-01-08 11:10:39 +0100

    💄 Add favicon

--- graph/src/index.ts
+++ graph/src/index.ts
@@ -1,8 +1,26 @@
-import {ApolloServer} from "apollo-server";
+import {ApolloServer} from "apollo-server-express";
+import * as express from "express";
+import * as favicon from "serve-favicon";
+import * as path from "path";
+import * as http from "http";
+
+const configurations = {
+    production: {port: 80, hostname: 'greedy-amigo.com'},
+    development: {port: 4000, hostname: 'localhost'}
+};
+
+const environment = process.env.NODE_ENV || 'production';
+const config = configurations[environment];
 
 const typeDefs = `
 type Query {
   info: String!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
@@ -12,11 +30,21 @@ const resolvers = {
     }
 };
 
-const server = new ApolloServer({
+const apolloServer = new ApolloServer({
     typeDefs,
     resolvers
 });
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+const app = express();
+apolloServer.applyMiddleware({app});
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
+let server = http.createServer(app)
+apolloServer.installSubscriptionHandlers(server);
+
+server.listen({port: config.port}, () =>
+    console.log(
+        '🚀 Server ready at',
+        `http://${config.hostname}:${config.port}${apolloServer.graphqlPath}`
+    )
+);
