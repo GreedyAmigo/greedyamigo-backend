dbedb8221a4cb3525d8f6b58581e84898bb83459
Simon Ammer <ammersimon@gmail.com>
2019-01-08 11:19:37 +0100

    🔧 Change server port

--- graph/src/index.ts
+++ graph/src/index.ts
@@ -4,9 +4,10 @@ import * as favicon from "serve-favicon";
 import * as path from "path";
 import * as http from "http";
 
+const port = 4000;
 const configurations = {
-    production: {port: 80, hostname: 'greedy-amigo.com'},
-    development: {port: 4000, hostname: 'localhost'}
+    production: {hostname: 'graph.greedy-amigo.com'},
+    development: {hostname: 'localhost'}
 };
 
 const environment = process.env.NODE_ENV || 'production';
@@ -42,9 +43,9 @@ app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 let server = http.createServer(app)
 apolloServer.installSubscriptionHandlers(server);
 
-server.listen({port: config.port}, () =>
+server.listen({port: port}, () =>
     console.log(
         '🚀 Server ready at',
-        `http://${config.hostname}:${config.port}${apolloServer.graphqlPath}`
+        `http://${config.hostname}${apolloServer.graphqlPath}`
     )
 );
