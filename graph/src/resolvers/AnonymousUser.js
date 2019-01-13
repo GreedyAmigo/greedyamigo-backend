a5a44fdfb082b0ca3f37848a7d1c6fb722fc7316
Simon Ammer <ammersimon@gmail.com>
2019-01-13 21:34:26 +0100

    Update gitignore

--- graph/src/resolvers/AnonymousUser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphqlgen_1 = require("../generated/graphqlgen");
-// tslint:disable-next-line:variable-name
-exports.AnonymousUser = Object.assign({}, graphqlgen_1.AnonymousUserResolvers.defaultResolvers, { user: (parent, args, ctx) => {
-        return ctx.prisma.anonymousUser({ id: parent.id }).user();
-    } });
-//# sourceMappingURL=AnonymousUser.js.map
